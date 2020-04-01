dfb8721ba3f74bab873817a6c2c6706d05a989bc
Jakub Fedak <fedaczynka@gmail.com>
2020-04-01 22:43:35 +0200

    Updated utility classes

--- src/main/java/Color.java
+++ src/main/java/Color.java
@@ -1,3 +1,14 @@
 public enum Color {
+    BLACK, WHITE, NONE;
 
+    public Color getOpposite() {   // returning opposite color
+        switch(this) {
+            case BLACK:
+                return Color.WHITE;
+            case WHITE:
+                return Color.BLACK;
+            default:
+                return Color.NONE;
+        }
+    }
 }
--- src/main/java/Position.java
+++ src/main/java/Position.java
@@ -1,3 +1,65 @@
 public class Position {
+    private int x;
+    private int y;
 
+    public Position() {
+        this(0, 0);
+    }
+
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public Position setX(int x) {
+        this.x = x;
+        return this;
+    }
+
+    public Position sexY(int y) {
+        this.y = y;
+        return this;
+    }
+
+    public Position setXY(int x, int y) {
+        this.x = x;
+        this.y = y;
+        return this;
+    }
+
+    public Position add(Position toAdd) {
+        return new Position(this.x + toAdd.x, this.y + toAdd.y);
+    }
+
+    public ArrayList<Position> getNeighbors() {
+        ArrayList<Position> ans = new ArrayList<>();
+        ans.add(new Position(x, y + 1));
+        ans.add(new Position(x - 1, y + 1));
+        ans.add(new Position(x - 1, y));
+        ans.add(new Position(x, y - 1));
+        ans.add(new Position(x + 1, y - 1));
+        ans.add(new Position(x + 1, y));
+        return ans;
+    }
+
+    public ArrayList<Position> getConnectedToBoth(Position second) {
+        ArrayList<Position> firstNeighbors = getNeighbors();
+        ArrayList<Position> secondNeighbors = second.getNeighbors();
+        firstNeighbors.retainAll(secondNeighbors);
+        return firstNeighbors;
+    }
+
+    public boolean equals(Object o) {
+        if(o == null || !(o instanceof Position))
+            return false;
+        return x == ((Position)o).x && y == ((Position)o).y;
+    }
 }

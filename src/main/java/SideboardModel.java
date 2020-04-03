bf0e1093f643debe9a477211591635d28e98b790
Filip Synowiec <filipsynowiec@MacBook-Pro-Filip.local>
2020-04-03 19:09:34 +0200

    Updated SideboardModel - methods get Position

--- src/main/java/SideboardModel.java
+++ src/main/java/SideboardModel.java
@@ -1,46 +1,47 @@
 import java.util.ArrayList;
 
 public class SideboardModel {
-    private ArrayList<TileModel> whiteList;
-    private ArrayList<TileModel> blackList;
+    private ArrayList<TileModel> whiteList; //(-1, y)
+    private ArrayList<TileModel> blackList; //(-2, y)
 
     public SideboardModel(){
         whiteList = new ArrayList<>();
         blackList = new ArrayList<>();
-        /*Adding Ant*/
-        whiteList.add(new TileAnt(Color.WHITE, new Position()));
-        blackList.add(new TileAnt(Color.BLACK, new Position()));
         /*Adding Bee*/
-        whiteList.add(new TileBee(Color.WHITE, new Position()));
-        blackList.add(new TileBee(Color.BLACK, new Position()));
+        whiteList.add(new TileBee(Color.WHITE, new Position(-1, 0)));
+        blackList.add(new TileBee(Color.BLACK, new Position(-2, 0)));
+        /*Adding Ant*/
+        whiteList.add(new TileAnt(Color.WHITE, new Position(-1, 1)));
+        blackList.add(new TileAnt(Color.BLACK, new Position(-2, 1)));
         /*Adding Grasshopper*/
-        whiteList.add(new TileGrasshopper(Color.WHITE, new Position()));
-        blackList.add(new TileGrasshopper(Color.BLACK, new Position()));
+        whiteList.add(new TileGrasshopper(Color.WHITE, new Position(-1, 2)));
+        blackList.add(new TileGrasshopper(Color.BLACK, new Position(-2, 2)));
         /*Adding Spider*/
-        whiteList.add(new TileSpider(Color.WHITE, new Position()));
-        blackList.add(new TileSpider(Color.BLACK, new Position()));
+        whiteList.add(new TileSpider(Color.WHITE, new Position(-1, 3)));
+        blackList.add(new TileSpider(Color.BLACK, new Position(-2, 3)));
     }
 
-    ArrayList<TileModel> chooseList(Color playerColor) {
-        if(playerColor == Color.BLACK) return blackList;
-        else return whiteList;
+    private ArrayList<TileModel> chooseList(Position pos) {
+        if(pos.getX() == -1) return whiteList;
+        else if(pos.getX() == -2) return blackList;
+        else return null; //throw exception?
     }
 
     //zwraca true, jezeli krolowa zostala wyrzucona
-    public boolean checkQueen(Color playerColor) { return !isPossible(new TileBee(playerColor, new Position())); }
 
+    public boolean checkQueenInPlay(Color playerColor) {
+        if(playerColor == Color.WHITE) return !isAvailable(new Position(-1, 0));
+        else return !isAvailable(new Position(-2, 0));
 
-    /* Ponizsze trzy funkcje w dwoch wersjach: jedna, w ktorej dostaje plytke i kolor, druga - sama plytka,
-    z ktorej wyciaga informacje o kolorze.
-     */
+    }
 
-    //Funkcja sprawdza, czy dostepne sa jeszcze dane plytki w danym kolorze
-    public boolean isPossible(TileModel tile, Color playerColor) {
+    //Funkcja sprawdza, czy dostepne sa jeszcze dane plytki.
 
-        ArrayList<TileModel> current = chooseList(playerColor);
+    public boolean isAvailable(Position pos) {
+        ArrayList<TileModel> current = chooseList(pos);
 
         for(TileModel element: current) {
-            if(element.getClass() == tile.getClass()) {
+            if(element.getPosition().equals(pos)) {
                 if(element.getCnt() >= 1) return true;
                 else return false;
             }
@@ -48,62 +49,42 @@ public class SideboardModel {
         return false;
     }
 
-    public boolean isPossible(TileModel tile) {
-        ArrayList<TileModel> current = chooseList(tile.getColor());
+    //Funkcja zwraca ile zostalo danych plytek. Jezeli nie ma takiej plytki w grze, zwraca -1
+
+    public int getTileCnt(Position pos) {
+        ArrayList<TileModel> current = chooseList(pos);
 
         for(TileModel element: current) {
-            if(element.getClass() == tile.getClass()) {
-                if(element.getCnt() >= 1) return true;
-                else return false;
-            }
-        }
-        return false;
-    }
-
-    //Funkcja zwraca ile zostalo danych plytek w danym kolorze. Jezeli nie ma takiej plytki w grze, zwraca -1
-    public int getTileCounter(TileModel tile, Color playerColor) {
-        ArrayList<TileModel> current = chooseList(playerColor);
-
-        for(TileModel element: current) {
-            if(element.getClass() == tile.getClass()) {
+            if(element.getPosition().equals(pos)) {
                 return element.getCnt();
             }
         }
-        return -1;
+        return -1; //throw exception?
     }
 
-    public int getTileCounter(TileModel tile) {
-        ArrayList<TileModel> current = chooseList(tile.getColor());
+    //Funckja zmniejsza ilosc danej plytki i zwraca aktualna wartosc. Jezeli nie ma plytki zwraca -1
+
+    public int decrementAndGetTileCnt(Position pos) {
+        ArrayList<TileModel> current = chooseList(pos);
 
         for(TileModel element: current) {
-            if(element.getClass() == tile.getClass()) {
-                return element.getCnt();
-            }
-        }
-        return -1;
-    }
-
-    //Funckja zmniejsza ilosc danej plytki w danym kolorze i zwraca aktualna wartosc. Jezeli nie ma plytki zwraca -1
-
-    public int decrementAndGetTileCnt(TileModel tile, Color playerColor) {
-        ArrayList<TileModel> current = chooseList(playerColor);
-
-        for(TileModel element: current) {
-            if(element.getClass() == tile.getClass()) {
+            if(element.getPosition().equals(pos)) {
                 return element.decrementAndGetCnt();
             }
         }
-        return -1;
+        return -1; //throw exception?
     }
 
-    public int decrementAndGetTileCnt(TileModel tile) {
-        ArrayList<TileModel> current = chooseList(tile.getColor());
+    //Funkcja zwraca plytke wg. pozycji
+    public TileModel getTile(Position pos) {
+        ArrayList<TileModel> current = chooseList(pos);
 
         for(TileModel element: current) {
-            if(element.getClass() == tile.getClass()) {
-                return element.decrementAndGetCnt();
+            if(element.getPosition().equals(pos)) {
+                return element;
             }
         }
-        return -1;
+        return null; //throw exception?
     }
 }
+//TODO: rzucanie wyjatkow?
--- src/test/java/SideboardModelTest.java
+++ src/test/java/SideboardModelTest.java
@@ -7,36 +7,36 @@ public class SideboardModelTest {
     @Test
     public void basicQueenTest() {
         SideboardModel sideboard = new SideboardModel();
-        assertFalse(sideboard.checkQueen(Color.BLACK));
-        assertFalse(sideboard.checkQueen(Color.WHITE));
+        assertFalse(sideboard.checkQueenInPlay(Color.BLACK));
+        assertFalse(sideboard.checkQueenInPlay(Color.WHITE));
 
-        TileModel tile = new TileBee(Color.BLACK, new Position());
+        TileModel tile = new TileBee(Color.BLACK, new Position(-2, 0));
 
-        assertTrue(sideboard.isPossible(tile, Color.BLACK));
+        assertTrue(sideboard.isAvailable(tile.getPosition()));
 
-        sideboard.decrementAndGetTileCnt(tile, Color.BLACK);
+        sideboard.decrementAndGetTileCnt(tile.getPosition());
 
-        assertFalse(sideboard.isPossible(tile, Color.BLACK));
-        assertTrue(sideboard.checkQueen(Color.BLACK));
-        assertFalse(sideboard.checkQueen(Color.WHITE));
+        assertFalse(sideboard.isAvailable(tile.getPosition()));
+        assertTrue(sideboard.checkQueenInPlay(Color.BLACK));
+        assertFalse(sideboard.checkQueenInPlay(Color.WHITE));
     }
 
     @Test
     public void basicCounterTest() {
         SideboardModel sideboard = new SideboardModel();
 
-        TileModel tileWhite = new TileBee(Color.WHITE, new Position());
+        TileModel tileWhite = new TileBee(Color.WHITE, new Position(-1, 0));
 
-        assertEquals(1, sideboard.getTileCounter(tileWhite));
-        sideboard.decrementAndGetTileCnt(tileWhite);
-        assertEquals(0, sideboard.getTileCounter(tileWhite));
+        assertEquals(1, sideboard.getTileCnt(tileWhite.getPosition()));
+        sideboard.decrementAndGetTileCnt(tileWhite.getPosition());
+        assertEquals(0, sideboard.getTileCnt(tileWhite.getPosition()));
 
-        tileWhite = new TileGrasshopper(Color.WHITE, new Position());
-        TileModel tileBlack = new TileGrasshopper(Color.BLACK, new Position());
+        tileWhite = new TileGrasshopper(Color.WHITE, new Position(-1 ,2));
+        TileModel tileBlack = new TileGrasshopper(Color.BLACK, new Position(-2, 2));
         for(int i = 3; i >= 0 ; i --) {
-            assertEquals(i, sideboard.getTileCounter(tileWhite));
-            assertEquals(3, sideboard.getTileCounter(tileBlack));
-            sideboard.decrementAndGetTileCnt(tileWhite);
+            assertEquals(i, sideboard.getTileCnt(tileWhite.getPosition()));
+            assertEquals(3, sideboard.getTileCnt(tileBlack.getPosition()));
+            sideboard.decrementAndGetTileCnt(tileWhite.getPosition());
         }
     }
 
@@ -44,44 +44,54 @@ public class SideboardModelTest {
     public void basicTileTest() {
         SideboardModel sideboard = new SideboardModel();
 
-        TileModel tileBee = new TileBee(Color.WHITE, new Position());
-        TileModel tileAnt = new TileAnt(Color.WHITE, new Position());
-        TileModel tileGrasshopper = new TileGrasshopper(Color.WHITE, new Position());
-        TileModel tileSpider = new TileSpider(Color.WHITE, new Position());
+        TileModel tileBee = new TileBee(Color.WHITE, new Position(-1, 0));
+        TileModel tileAnt = new TileAnt(Color.WHITE, new Position(-1, 1));
+        TileModel tileGrasshopper = new TileGrasshopper(Color.WHITE, new Position(-1, 2));
+        TileModel tileSpider = new TileSpider(Color.WHITE, new Position(-1, 3));
 
-        assertEquals(1, sideboard.getTileCounter(tileBee));
-        assertEquals(3, sideboard.getTileCounter(tileAnt));
-        assertEquals(3, sideboard.getTileCounter(tileGrasshopper));
-        assertEquals(2, sideboard.getTileCounter(tileSpider));
+        assertEquals(1, sideboard.getTileCnt(tileBee.getPosition()));
+        assertEquals(3, sideboard.getTileCnt(tileAnt.getPosition()));
+        assertEquals(3, sideboard.getTileCnt(tileGrasshopper.getPosition()));
+        assertEquals(2, sideboard.getTileCnt(tileSpider.getPosition()));
 
-        TileModel blackGrasshopper = new TileGrasshopper(Color.BLACK, new Position());
+        TileModel blackGrasshopper = new TileGrasshopper(Color.BLACK, new Position(-2, 2));
         int cnt = 0;
-        while (sideboard.isPossible(blackGrasshopper)) {
+        while (sideboard.isAvailable(blackGrasshopper.getPosition())) {
             cnt++;
-            sideboard.decrementAndGetTileCnt(blackGrasshopper);
+            sideboard.decrementAndGetTileCnt(blackGrasshopper.getPosition());
         }
         assertEquals(3, cnt);
-        assertEquals(3, sideboard.getTileCounter(tileGrasshopper));
+        assertEquals(3, sideboard.getTileCnt(tileGrasshopper.getPosition()));
     }
 
     @Test
     public void basicDecrementTest() {
         SideboardModel sideboard = new SideboardModel();
 
-        TileModel tileBee = new TileBee(Color.WHITE, new Position());
-        TileModel tileAnt = new TileAnt(Color.WHITE, new Position());
-        TileModel tileGrasshopper = new TileGrasshopper(Color.WHITE, new Position());
+        TileModel tileBee = new TileBee(Color.WHITE, new Position(-1, 0));
+        TileModel tileAnt = new TileAnt(Color.WHITE, new Position(-1, 1));
+        TileModel tileGrasshopper = new TileGrasshopper(Color.WHITE, new Position(-1, 2));
 
-        assertFalse(sideboard.checkQueen(Color.WHITE));
-        assertEquals(0, sideboard.decrementAndGetTileCnt(tileBee));
-        assertTrue(sideboard.checkQueen(Color.WHITE));
+        assertFalse(sideboard.checkQueenInPlay(Color.WHITE));
+        assertEquals(0, sideboard.decrementAndGetTileCnt(tileBee.getPosition()));
+        assertTrue(sideboard.checkQueenInPlay(Color.WHITE));
 
-        assertEquals(2, sideboard.decrementAndGetTileCnt(tileAnt));
-        assertEquals(1, sideboard.decrementAndGetTileCnt(tileAnt));
-        assertTrue(sideboard.isPossible(tileAnt));
-        assertEquals(0, sideboard.decrementAndGetTileCnt(tileAnt));
-        assertFalse(sideboard.isPossible(tileAnt));
-        assertEquals(3, sideboard.getTileCounter(tileGrasshopper));
+        assertEquals(2, sideboard.decrementAndGetTileCnt(tileAnt.getPosition()));
+        assertEquals(1, sideboard.decrementAndGetTileCnt(tileAnt.getPosition()));
+        assertTrue(sideboard.isAvailable(tileAnt.getPosition()));
+        assertEquals(0, sideboard.decrementAndGetTileCnt(tileAnt.getPosition()));
+        assertFalse(sideboard.isAvailable(tileAnt.getPosition()));
+        assertEquals(3, sideboard.getTileCnt(tileGrasshopper.getPosition()));
+    }
 
+    @Test
+    public void basicGetTileTest() {
+        SideboardModel sideboard = new SideboardModel();
+
+        TileModel tileBee = new TileBee(Color.WHITE, new Position(-1, 0));
+        TileModel tileAnt = new TileAnt(Color.WHITE, new Position(-1, 1));
+
+        assertTrue(tileAnt.getClass() == sideboard.getTile(tileAnt.getPosition()).getClass());
+        assertTrue(tileBee.getClass() == sideboard.getTile(tileBee.getPosition()).getClass());
     }
 }

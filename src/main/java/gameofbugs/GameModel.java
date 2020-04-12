5d70b264b9e2dfb04c292556c7f50b0b0683237e
Michał Woźny <mbwozny@gmail.com>
2020-04-12 22:13:31 +0200

    Updated GameModel to run in two phases

--- src/main/java/gameofbugs/GameModel.java
+++ src/main/java/gameofbugs/GameModel.java
@@ -1,98 +1,101 @@
 package gameofbugs;
 import gameofbugs.tiles.*;
 
-import java.util.ArrayList;
-
-
 public class GameModel {
-    BoardModel myBoard;
-    SideboardModel mySideboard;
+    BoardModel board;
+    SideboardModel sideboard;
     Color currentPlayer;
-    int turns;
     Position lastPosition;
+    int turn;
 
     public GameModel() {
-        myBoard = new BoardModel();
-        mySideboard = new SideboardModel();
-        currentPlayer = Color.WHITE; //White player starts game
+        board = new BoardModel();
+        sideboard = new SideboardModel();
+        currentPlayer = Color.WHITE;
         lastPosition = null;
-        turns = 1;  // starting with first turn
+        turn = 1;
+    }
+
+    public void takeAction(Position pos) {
+        if(lastPosition == null)
+            runFirstPhase(pos);
+        else
+            runSecondPhase(pos);
+    }
+
+    // sets lastPosition and placeholders
+    private void runFirstPhase(Position pos) {
+        if(pos.getX() == -1 || pos.getX() == -2) {
+            if(sideboard.getColor(pos) != currentPlayer) return;
+            if(!sideboard.isAvailable(pos)) return;
+            if(turn > 3 && !sideboard.checkQueenInPlay(currentPlayer) &&
+                    !(sideboard.getTile(pos) instanceof TileBee)) return;
+
+            if(turn == 1) board.markHexesForFirstTile(currentPlayer);
+            else board.markHexesForNewTile(currentPlayer);
+
+            if(!board.containsPlaceholders()) return;
+
+            lastPosition = pos;
+        } else {
+            if(!sideboard.checkQueenInPlay(currentPlayer)) return;
+            if(!board.checkColor(pos, currentPlayer)) return;
+            if(!board.staysConnected(pos)) return;
+
+            board.markHexesForTileMovement(pos);
+            if(!board.containsPlaceholders()) return;
+
+            lastPosition = pos;
+            //TODO: Update view with placeholders
+        }
+    }
+
+    private void runSecondPhase(Position pos) {
+        if(!board.isPlaceholder(pos)) {
+            board.cleanPlaceholders();
+            lastPosition = null;
+            return;
+        }
+
+        board.cleanPlaceholders();
+
+        if(lastPosition.getX() == -1 || lastPosition.getX() == -2) {
+            TileModel currentTile = sideboard.getTile(lastPosition);
+            currentTile.setPosition(pos);
+            board.addNewTile(currentTile);
+            sideboard.decrementAndGetTileCnt(pos);
+        } else {
+            board.moveTile(lastPosition, pos);
+        }
+
+        lastPosition = null;
+
+        nextTurn();
+
+        //TODO: Check end game condition
+
+        //TODO: Update view: sideboard and board
+    }
+
+    private void nextTurn() {
+        if(board.checkForRebuild())
+            board.rebuildBoard();
+
+        currentPlayer = currentPlayer.getOpposite();
+
+        if(currentPlayer == Color.WHITE)
+            turn++;
     }
 
     public int endGameCondition(){
         boolean flagWhite = false;
         boolean flagBlack = false;
-        if(myBoard.isQueenSurrounded(Color.WHITE)) flagWhite = true;
-        if(myBoard.isQueenSurrounded(Color.BLACK)) flagBlack = true;
+        if(board.isQueenSurrounded(Color.WHITE)) flagWhite = true;
+        if(board.isQueenSurrounded(Color.BLACK)) flagBlack = true;
 
         if(flagBlack && flagWhite) return -1; //Both lost
         else if(flagBlack) return 2;  //Black lost
         else if(flagWhite) return 1;  //White lost
         else return 0; //No one lost
     }
-
-    public void nextTurn() {
-        currentPlayer = currentPlayer.getOpposite();
-        turns++;
-    }
-
-    public boolean action(Position pos) { //false if failed action, turns++ only when second action succeeds
-        if(myBoard.containsPlaceholders()) return actionWithPlaceholders(pos);
-        else{
-            boolean flag = actionWithoutPlaceholders(pos);
-            if(flag) nextTurn();                            //TODO: Give new Board to update
-            else myBoard.cleanPlaceholders();
-            return flag;
-        }
-    }
-
-    private boolean actionWithPlaceholders(Position pos) {
-        if(!myBoard.isPlaceholder(pos)) return false;
-        myBoard.cleanPlaceholders();
-        if(lastPosition.getX() == -1 || lastPosition.getX() == -2) {
-            TileModel currentTile = getNew(lastPosition, currentPlayer);
-            myBoard.addNewTile(currentTile);
-            mySideboard.decrementAndGetTileCnt(pos);           //TODO: Give new cnt to update
-        }
-        else {
-            myBoard.moveTile(lastPosition, pos);
-        }
-        return true;
-    }
-
-    //returning new tile from sideboard
-    private TileModel getNew(Position pos, Color col) {
-        if(pos.getY() == 0) return new TileBee(col, pos);
-        if(pos.getY() == 1) return new TileAnt(col, pos);
-        if(pos.getY() == 2) return new TileGrasshopper(col, pos);
-        if(pos.getY() == 3) return new TileSpider(col, pos);
-        return null;
-    }
-
-    //sets lastPosition and placeholders
-    private boolean actionWithoutPlaceholders(Position pos) {
-        if(pos.getX() == -1 || pos.getX() == -2) {
-            if(mySideboard.getColor(pos) != currentPlayer) return false;
-            if(!mySideboard.isAvailable(pos)) return false;
-            if(turns > 6 && !mySideboard.checkQueenInPlay(currentPlayer) && pos.getY() != 0) return false;
-            lastPosition = pos;
-            myBoard.markHexesForNewTile(currentPlayer);
-        }
-        else {
-            if(!mySideboard.checkQueenInPlay(currentPlayer)) return false;
-            if(!myBoard.checkColor(pos, currentPlayer)) return false;
-            if(!myBoard.staysConnected(pos)) return false;
-
-            //check if position is "locked"
-            ArrayList<Position> neighborsList = pos.getNeighbors();
-            for(Position p: neighborsList) if(myBoard.isEmpty(p)) neighborsList.remove(p);
-            if(neighborsList.size()>5) return false;
-
-            myBoard.markHexesForTileMovement(pos);
-            if(!myBoard.containsPlaceholders()) return false;
-            lastPosition = pos;                                 //TODO: Give Placeholders to update
-        }
-        return true;
-    }
-
 }
